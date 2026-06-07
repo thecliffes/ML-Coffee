a816ba2d094702fd5b0067bd4fc4a4ba33205a38
Ben Cliffe <ben@thecliffes.co.uk>
2026-06-07 18:08:41 +0100

    added ability to go through reviews dynamically

--- Scraper.py
+++ Scraper.py
@@ -2,13 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import polars as pl
 
-url = "https://www.coffeereview.com/review/costa-rica-mirazu-catajo-geisha-blend/"
+url = "https://www.coffeereview.com/review/"
 response = requests.get(url)
 
 html = response.text
 
 
 soup = BeautifulSoup(html, "html.parser")
+
+def get_review(soup, selector):
+    element = soup.select_one(selector)
+    return element['href'] if element else None
 
 def get_text(soup, selector):
     element = soup.select_one(selector)
@@ -32,37 +36,46 @@ def table_to_dict(table):
     return data
 
 
+def get_data(soup):
+    labels = {}
 
-labels = {}
+    labels["Score"] = get_text(
+        soup,
+        "#genesis-content article div.row.row-1 div.column.col-1 span"
+    )
 
-labels["Score"] = get_text(
-    soup,
-    "#genesis-content article div.row.row-1 div.column.col-1 span"
-)
+    labels["Name"] = get_text(soup, "h1")
 
-labels["Name"] = get_text(soup, "h1")
+    col1 = table_to_dict(
+        soup.select_one("div.column.col-1 table.review-template-table")
+    )
 
-col1 = table_to_dict(
-    soup.select_one("div.column.col-1 table.review-template-table")
-)
+    col2 = table_to_dict(
+        soup.select_one("div.column.col-2 table.review-template-table")
+    )
 
-col2 = table_to_dict(
-    soup.select_one("div.column.col-2 table.review-template-table")
-)
+    coffee_data = {
+        "name": labels.get("Name"),
+        "score": labels.get("Score"),
+        "roast_level": col1.get("Roast Level"),
+        "aroma": col2.get("Aroma"),
+        "structure": col2.get("Acidity/Structure"),
+        "body": col2.get("Body"),
+        "flavour": col2.get("Flavor"),
+        "aftertaste": col2.get("Aftertaste"),
+    }
+    return coffee_data
 
-coffee_data = {
-    "name": labels.get("Name"),
-    "score": labels.get("Score"),
-    "roast_level": col1.get("Roast Level"),
-    "aroma": col2.get("Aroma"),
-    "structure": col2.get("Acidity/Structure"),
-    "body": col2.get("Body"),
-    "flavour": col2.get("Flavor"),
-    "aftertaste": col2.get("Aftertaste"),
-}
+url = get_review(soup, "h2.review-title a")
+response = requests.get(url)
+
+html = response.text
 
 
+soup = BeautifulSoup(html, "html.parser")
 
+coffee_data = get_data(soup)
+print(coffee_data)
 df = pl.DataFrame(coffee_data)
 df = df.with_columns([pl.col('score').cast(pl.Int8), 
                       pl.col('aroma').cast(pl.Int8),
