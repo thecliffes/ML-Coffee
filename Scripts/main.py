c58931da390ac4d6fcb9eef8e2f0d7b9dbc91195
Ben Cliffe <ben@thecliffes.co.uk>
2026-06-20 11:48:20 +0100

    crrated start of streamlit app

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,11 +2,18 @@ import joblib
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-preprocessor = joblib.load('preprocessor.joblib')
-X = joblib.load('feature_matrix.joblib')
-df = pd.read_csv('coffee_names.csv', index_col=0)
+preprocessor = joblib.load('data/preprocessor.joblib')
+print("Preprocessor loaded successfully.")
+weight_vector = joblib.load('data/weight_vector.joblib')
+print("Weight vector loaded successfully.")
+X = joblib.load('data/feature_matrix.joblib')
+df = pd.read_csv('data/coffee_names.csv', index_col=0)
+print("Data loaded successfully.")
 
 # weight_vector needs to be rebuilt or also saved
 import numpy as np
-joblib.dump(weight_vector, 'weight_vector.joblib')
-weight_vector = joblib.load('weight_vector.joblib')
+joblib.dump(weight_vector, 'data/weight_vector.joblib')
+weight_vector = joblib.load('data/weight_vector.joblib')
+print("Weight vector rebuilt and loaded successfully.")
+
+
